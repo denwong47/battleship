e187d26960529d6f0d189f32e3d0184a0d304c92
denwong47 <denwong47@users.noreply.github.com>
2023-08-14 23:46:55 +0000

    deploy: 12010e81b25bb8dafe3bb676e0de387ec5c3b04f

--- battleship/app/hooks/sidebar-items.js
+++ battleship/app/hooks/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["app_status","board_status","drop_board","list_boards","new_board","strike","termination"],"struct":["AppStatusHook","BoardStatusHook","DropBoardHook","ListBoardsHook","NewBoardHook","StrikeHook","TerminationHook"]};
+window.SIDEBAR_ITEMS = {"fn":["should_fail"],"mod":["app_status","board_status","drop_board","list_boards","new_board","simulated_failure","strike","termination"],"struct":["AppStatusHook","BoardStatusHook","DropBoardHook","ListBoardsHook","NewBoardHook","SimulatedFailureHook","StrikeHook","TerminationHook"]};
