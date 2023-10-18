415d027a7c8318ac0dfed2a3359c8d82d46c3ebe
denwong47 <denwong47@users.noreply.github.com>
2023-10-18 10:52:30 +0000

    deploy: aed21cfbffec35ed6b3ff0eece066143b1859a59

--- implementors/core/error/trait.Error.js
+++ implementors/core/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"battleship":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"battleship/error/enum.AppError.html\" title=\"enum battleship::error::AppError\">AppError</a>"]]
+"battleship":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"battleship/error/enum.AppError.html\" title=\"enum battleship::error::AppError\">AppError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
