43ce61e9e2bd418824ca545ac26ac93e9fc445b2
denwong47 <denwong47@users.noreply.github.com>
2024-01-23 22:50:50 +0000

    deploy: 0b2026ec48b02019ca0faaa72d4f5967206314a1

--- implementors/core/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"battleship":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"battleship/error/enum.AppError.html\" title=\"enum battleship::error::AppError\">AppError</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/fmt/trait.Display.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"battleship":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"battleship/error/enum.AppError.html\" title=\"enum battleship::error::AppError\">AppError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"battleship/models/position/enum.Position.html\" title=\"enum battleship::models::position::Position\">Position</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
